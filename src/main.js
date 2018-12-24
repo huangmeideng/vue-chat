9903fabceea921c3c811cf1f2431564963a5e278
huangmeideng <huangmeideng@outlook.com>
2018-12-24 18:38:58 +0800

    引入vant框架

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
+Vue.use(Vant)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <van-nav-bar title="vue聊" fixed class="nav-header"></van-nav-bar>
+        <van-tabbar v-model="active">
+            <van-tabbar-item icon="chat-o">聊天</van-tabbar-item>
+            <van-tabbar-item icon="phone-circle-o" dot>通讯录</van-tabbar-item>
+            <van-tabbar-item icon="underway-o" info="2">社区</van-tabbar-item>
+            <van-tabbar-item icon="user-circle-o" info="20">我</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script>
+import { NavBar, Tabbar, TabbarItem } from 'vant'
+export default {
+    name: "Home",
+    components: {
+        NavBar,
+        Tabbar,
+        TabbarItem
+    },
+    data() {
+        return {
+            active: 1
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .nav-header {
+        background-color: #4494D5;
+        color: #FFFFFF;
+    }
+</style>
+
